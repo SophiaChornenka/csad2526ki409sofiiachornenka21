1fda057247bb07b9562b67eca5c39f06a2edb243
SophiaChornenka <sofiia.chornenka.ki.2022@lpnu.ua>
2025-10-18 00:23:59 +0300

    Update unit_tests.cpp

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -3,6 +3,12 @@
 
 // Basic tests for add(int, int)
 
+
+// Simple sample test - replace/add real tests here
+TEST(SampleTest, Addition) {
+  EXPECT_EQ(2 + 2, 4);
+}
+
 TEST(AddTest, PositiveNumbers) {
     EXPECT_EQ(add(1, 2), 3);
     EXPECT_EQ(add(100, 200), 300);
